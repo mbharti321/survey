8d1fee14907ec1369019fe0d67b1e00152868ce6
mbharti321 <mbharti321@gmail.com>
2022-05-11 22:18:13 +0530

    add question template

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const dbURL = "mongodb://localhost:27017";
+mongoose.connect(dbURL + "/surveyDB");
+
+const questionSchema = mongoose.Schema({
+    question: "",
+    answer: "option a",
+    answer: {
+        a: "option a",
+        b: "option b",
+        c: "option c",
+        d: "option d",
+    }
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+const questionTemp = new Question({
+    question: "Captial of India:",
+    answer: "Delhi",
+    answer: {
+        a: "Delhi",
+        b: "Chennai",
+        c: "Mumbai",
+        d: "Bengaluru",
+    }
+});
+
+const questions = [questionTemp];
 
 
 
@@ -19,17 +47,15 @@ app.use(express.static("public"));
 
 
 
-
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     // res.send("Hello World!");
     res.render("index");
 });
 
-app.get("/admin", (req, res)=>{
+app.get("/admin", (req, res) => {
     res.render("admin/admin")
 });
 
-app.listen("3000", ()=>{
+app.listen("3000", () => {
     console.log("Server started!! at http://localhost:3000/");
 });
